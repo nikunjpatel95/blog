c5bd94e03c6b339f9c49dc0a09241ecfb7043de2
Nikunj Patel <nikunjmbpatel@gmail.com>
2024-04-08 06:54:05 -0400

    each post is seperated

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -5,4 +5,9 @@ from .models import Post
 def index(request):
     posts=Post.objects.all()
     n=len(posts)
-    return render(request,'index.html',{'posts': posts, 'length':n})
+    return render(request,'index.html',{'posts': posts})
+
+
+def post(request,pk):
+    posts=Post.objects.get(id=pk)  ##Post is the model name, it is going to get the particular post, which has id of pk
+    return render(request,'posts.html',{'posts': posts})
